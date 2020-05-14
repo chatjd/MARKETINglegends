160cbac56d523ed44a65aa51a60bfd298f86274b
chatjd <125004618+chatjd@users.noreply.github.com>
2020-05-14 12:51:49 +0000

    Create nodejs.yml

--- qa/rpc-tests/getblocktemplate.py
+++ qa/rpc-tests/getblocktemplate.py
@@ -21,4 +21,40 @@ class GetBlockTemplateTest(BitcoinTestFramework):
     def setup_network(self, split=False):
         self.nodes = start_nodes(2, self.options.tmpdir)
         connect_nodes_bi(self.nodes,0,1)
-        
+        self.is_network_split=False
+        self.sync_all()
+
+    def run_test(self):
+        node = self.nodes[0]
+        node.generate(1) # Mine a block to leave initial block download
+
+        # Test 1: Default to coinbasetxn
+        tmpl = node.getblocktemplate()
+        assert('coinbasetxn' in tmpl)
+        assert('coinbasevalue' not in tmpl)
+
+        # Test 2: Get coinbasetxn if requested
+        tmpl = node.getblocktemplate({'capabilities': ['coinbasetxn']})
+        assert('coinbasetxn' in tmpl)
+        assert('coinbasevalue' not in tmpl)
+
+        # Test 3: coinbasevalue not supported if requested
+        tmpl = node.getblocktemplate({'capabilities': ['coinbasevalue']})
+        assert('coinbasetxn' in tmpl)
+        assert('coinbasevalue' not in tmpl)
+
+        # Test 4: coinbasevalue not supported if both requested
+        tmpl = node.getblocktemplate({'capabilities': ['coinbasetxn', 'coinbasevalue']})
+        assert('coinbasetxn' in tmpl)
+        assert('coinbasevalue' not in tmpl)
+
+        # Test 5: General checks
+        tmpl = node.getblocktemplate()
+        assert(len(tmpl['noncerange']) == 16)
+
+        # Test 6: coinbasetxn checks
+        assert('vrewardsystem' in tmpl['coinbasetxn'])
+        assert(tmpl['coinbasetxn']['required'])
+
+if __name__ == '__main__':
+    GetBlockTemplateTest().main()
---
+++ qa/rpc-tests/getchaintips.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python2
+# Copyright (c) 2014 The Bitcoin Core developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+# Exercise the getchaintips API.  We introduce a network split, work
+# on chains of different lengths, and join the network together again.
+# This gives us two tips, verify that it works.
+
+from test_framework.test_framework import BitcoinTestFramework
+from test_framework.util import assert_equal
+
+class GetChainTipsTest (BitcoinTestFramework):
+
+    def run_test (self):
+        BitcoinTestFramework.run_test (self)
+
+        tips = self.nodes[0].getchaintips ()
+        assert_equal (len (tips), 1)
+        assert_equal (tips[0]['branchlen'], 0)
+        assert_equal (tips[0]['height'], 200)
+        assert_equal (tips[0]['status'], 'active')
+
+        # Split the network and build two c
