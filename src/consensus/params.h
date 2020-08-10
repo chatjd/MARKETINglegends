c99c580ecbaf8da6e51247681bd43ba549ca576a
chatjd <125004618+chatjd@users.noreply.github.com>
2020-08-10 07:09:36 +0000

    2.3

--- src/consensus/params.h
+++ src/consensus/params.h
@@ -19,4 +19,106 @@ namespace Consensus {
  *
  * Being array indices, these MUST be numbered consecutively.
  *
- * The order of these indices MUST match the order of the upg
+ * The order of these indices MUST match the order of the upgrades on-chain, as
+ * several functions depend on the enum being sorted.
+ */
+enum UpgradeIndex {
+    // Sprout must be first
+    BASE_SPROUT,
+    UPGRADE_TESTDUMMY,
+    UPGRADE_OVERWINTER,
+    UPGRADE_SAPLING,
+    UPGRADE_DIFA,
+    UPGRADE_DENNIS,
+    UPGRADE_LIQUID,
+    // NOTE: Also add new upgrades to NetworkUpgradeInfo in upgrades.cpp
+    MAX_NETWORK_UPGRADES
+};
+
+struct NetworkUpgrade {
+    /**
+     * The first protocol version which will understand the new consensus rules
+     */
+    int nProtocolVersion;
+
+    /**
+     * Height of the first block for which the new consensus rules will be active
+     */
+    int nActivationHeight;
+
+    /**
+     * Special value for nActivationHeight indicating that the upgrade is always active.
+     * This is useful for testing, as it means tests don't need to deal with the activation
+     * process (namely, faking a chain of somewhat-arbitrary length).
+     *
+     * New blockchains that want to enable upgrade rules from the beginning can also use
+     * this value. However, additional care must be taken to ensure the genesis block
+     * satisfies the enabled rules.
+     */
+    static constexpr int ALWAYS_ACTIVE = 0;
+
+    /**
+     * Special value for nActivationHeight indicating that the upgrade will never activate.
+     * This is useful when adding upgrade code that has a testnet activation height, but
+     * should remain disabled on mainnet.
+     */
+    static constexpr int NO_ACTIVATION_HEIGHT = -1;
+};
+
+/**
+ * Parameters that influence chain consensus.
+ */
+struct Params {
+    uint256 hashGenesisBlock;
+
+    bool fCoinbaseMustBeProtected;
+
+    /** Needs to evenly divide MAX_SUBSIDY to avoid rounding errors. */
+    int nSubsidySlowStartInterval;
+    /**
+     * Shift based on a linear ramp for slow start:
+     *
+     * MAX_SUBSIDY*(t_s/2 + t_r) = MAX_SUBSIDY*t_h  Coin balance
+     *              t_s   + t_r  = t_h + t_c        Block balance
+     *
+     * t_s = nSubsidySlowStartInterval
+     * t_r = number of blocks between end of slow start and first halving
+     * t_h = nSubsidyHalvingInterval
+     * t_c = SubsidySlowStartShift()
+     */
+    //int SubsidySlowStartShift() const { return nSubsidySlowStartInterval / 2; }
+    int nSubsidyHalvingInterval;
+    int GetLastVRewardSystemBlockHeight() const {
+        //return nSubsidyHalvingInterval + SubsidySlowStartShift() - 1;
+        return 99999999;
+    }
+
+    // int GetVRewardSystemRepeatInterval() const {
+    //     return nSubsidyHalvingInterval + SubsidySlowStartShift() - 1;
+    // }
+    /** Used to check majorities for block version upgrade */
+    int nMajorityEnforceBlockUpgrade;
+    int nMajorityRejectBlockOutdated;
+    int nMajorityWindow;
+    NetworkUpgrade vUpgrades[MAX_NETWORK_UPGRADES];
+    /** Proof of work parameters */
+    uint256 powLimit;
+    boost::optional<uint32_t> nPowAllowMinDifficultyBlocksAfterHeight;
+    int64_t nPowAveragingWindow;
+    int64_t nPowMaxAdjustDown;
+    int64_t nPowMaxAdjustUp;
+    int64_t nPowTargetSpacing;
+    int64_t nSelfishMiningDepth; // how many blocks back to check for selfish mining
+    int nMasternodePaymentsStartBlock;
+    int nMasternodePaymentsIncreasePeriod; // in blocks
+    int64_t AveragingWindowTimespan() const { return nPowAveragingWindow * nPowTargetSpacing; }
+    int64_t MinActualTimespan() const { return (AveragingWindowTimespan() * (100 - nPowMaxAdjustUp  )) / 100; }
+    int64_t MaxActualTimespan() const { return (AveragingWindowTimespan() * (100 + nPowMaxAdjustDown)) / 100; }
+    uint256 nMinimumChainWork;
+
+    /** Parameters for LWMA3 **/
+    int64_t nZawyLWMA3AveragingWindow;  // N
+};
+} // namespace Consensus
+
+#endif // BITCOIN_CONSENSUS_PARAMS_H
