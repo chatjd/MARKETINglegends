c080404c0a47d0b07891093295f05567ad7b1e3a
chatjd <125004618+chatjd@users.noreply.github.com>
2020-08-18 09:43:10 +0000

    Adjustments

--- src/crypto/hmac_sha512.h
+++ src/crypto/hmac_sha512.h
@@ -14,4 +14,19 @@
 class CHMAC_SHA512
 {
 private:
-    CSH
+    CSHA512 outer;
+    CSHA512 inner;
+
+public:
+    static const size_t OUTPUT_SIZE = 64;
+
+    CHMAC_SHA512(const unsigned char* key, size_t keylen);
+    CHMAC_SHA512& Write(const unsigned char* data, size_t len)
+    {
+        inner.Write(data, len);
+        return *this;
+    }
+    void Finalize(unsigned char hash[OUTPUT_SIZE]);
+};
+
+#endif // BITCOIN_CRYPTO_HMAC_SHA512_H
