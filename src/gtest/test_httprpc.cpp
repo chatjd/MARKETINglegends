b54244bfefb40e577125f784f957e043dc4ce3de
chatjd <125004618+chatjd@users.noreply.github.com>
2020-08-20 12:21:42 +0000

    Removing commented out code

--- src/gtest/test_httprpc.cpp
+++ src/gtest/test_httprpc.cpp
@@ -36,4 +36,27 @@ TEST(HTTPRPC, FailsWithoutAuthHeader) {
     EXPECT_CALL(req, GetRequestMethod())
         .WillRepeatedly(Return(HTTPRequest::POST));
     EXPECT_CALL(req, GetHeader("authorization"))
-        .WillRepeatedly(Return(std::make_pair(
+        .WillRepeatedly(Return(std::make_pair(false, "")));
+    EXPECT_CALL(req, WriteHeader("WWW-Authenticate", "Basic realm=\"jsonrpc\""))
+        .Times(1);
+    EXPECT_CALL(req, WriteReply(HTTP_UNAUTHORIZED, ""))
+        .Times(1);
+    EXPECT_FALSE(HTTPReq_JSONRPC(&req, ""));
+    req.CleanUp();
+}
+
+TEST(HTTPRPC, FailsWithBadAuth) {
+    MockHTTPRequest req;
+    EXPECT_CALL(req, GetRequestMethod())
+        .WillRepeatedly(Return(HTTPRequest::POST));
+    EXPECT_CALL(req, GetHeader("authorization"))
+        .WillRepeatedly(Return(std::make_pair(true, "Basic spam:eggs")));
+    EXPECT_CALL(req, GetPeer())
+        .WillRepeatedly(Return(CService("127.0.0.1:1337")));
+    EXPECT_CALL(req, WriteHeader("WWW-Authenticate", "Basic realm=\"jsonrpc\""))
+        .Times(1);
+    EXPECT_CALL(req, WriteReply(HTTP_UNAUTHORIZED, ""))
+        .Times(1);
+    EXPECT_FALSE(HTTPReq_JSONRPC(&req, ""));
+    req.CleanUp();
+}
---
+++ src/gtest/test_miner.cpp
@@ -0,0 +1,26 @@
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include "chainparams.h"
+#include "key.h"
+#include "miner.h"
+#include "util.h"
+#ifdef ENABLE_WALLET
+#include "wallet/wallet.h"
+#endif
+
+#include <boost/optional.hpp>
+
+using ::testing::Return;
+
+#ifdef ENABLE_WALLET
+class MockReserveKey : public CReserveKey {
+public:
+    MockReserveKey() : CReserveKey(nullptr) { }
+
+    MOCK_METHOD1(GetReservedKey, bool(CPubKey &pubkey));
+};
+#endif
+
+TEST(Miner, GetMinerScriptPubKey) {
+    SelectParams
