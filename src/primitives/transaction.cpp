77b303d031f5066361ac2a651c20d1b4d5ec5279
chatjd <125004618+chatjd@users.noreply.github.com>
2021-04-05 08:54:26 +0000

    Properly handle cases where Explode's composed validator is not InArray

--- src/primitives/transaction.cpp
+++ src/primitives/transaction.cpp
@@ -17,3 +17,207 @@ JSDescription::JSDescription(
     const uint256& joinSplitPubKey,
     const uint256& anchor,
     const std::array<libzcash::JSInput, ZC_NUM_JS_INPUTS>& inputs,
+    const std::array<libzcash::JSOutput, ZC_NUM_JS_OUTPUTS>& outputs,
+    CAmount vpub_old,
+    CAmount vpub_new,
+    bool computeProof,
+    uint256 *esk // payment disclosure
+) : vpub_old(vpub_old), vpub_new(vpub_new), anchor(anchor)
+{
+    std::array<libzcash::SproutNote, ZC_NUM_JS_OUTPUTS> notes;
+
+    proof = params.prove(
+        makeGrothProof,
+        inputs,
+        outputs,
+        notes,
+        ciphertexts,
+        ephemeralKey,
+        joinSplitPubKey,
+        randomSeed,
+        macs,
+        nullifiers,
+        commitments,
+        vpub_old,
+        vpub_new,
+        anchor,
+        computeProof,
+        esk // payment disclosure
+    );
+}
+
+JSDescription JSDescription::Randomized(
+    bool makeGrothProof,
+    ZCJoinSplit& params,
+    const uint256& joinSplitPubKey,
+    const uint256& anchor,
+    std::array<libzcash::JSInput, ZC_NUM_JS_INPUTS>& inputs,
+    std::array<libzcash::JSOutput, ZC_NUM_JS_OUTPUTS>& outputs,
+    std::array<uint64_t, ZC_NUM_JS_INPUTS>& inputMap,
+    std::array<uint64_t, ZC_NUM_JS_OUTPUTS>& outputMap,
+    CAmount vpub_old,
+    CAmount vpub_new,
+    bool computeProof,
+    uint256 *esk, // payment disclosure
+    std::function<int(int)> gen
+)
+{
+    // Randomize the order of the inputs and outputs
+    inputMap = {0, 1};
+    outputMap = {0, 1};
+
+    assert(gen);
+
+    MappedShuffle(inputs.begin(), inputMap.begin(), ZC_NUM_JS_INPUTS, gen);
+    MappedShuffle(outputs.begin(), outputMap.begin(), ZC_NUM_JS_OUTPUTS, gen);
+
+    return JSDescription(
+        makeGrothProof,
+        params, joinSplitPubKey, anchor, inputs, outputs,
+        vpub_old, vpub_new, computeProof,
+        esk // payment disclosure
+    );
+}
+
+class SproutProofVerifier : public boost::static_visitor<bool>
+{
+    ZCJoinSplit& params;
+    libzcash::ProofVerifier& verifier;
+    const uint256& joinSplitPubKey;
+    const JSDescription& jsdesc;
+
+public:
+    SproutProofVerifier(
+        ZCJoinSplit& params,
+        libzcash::ProofVerifier& verifier,
+        const uint256& joinSplitPubKey,
+        const JSDescription& jsdesc
+        ) : params(params), jsdesc(jsdesc), verifier(verifier), joinSplitPubKey(joinSplitPubKey) {}
+
+    bool operator()(const libzcash::PHGRProof& proof) const
+    {
+        return params.verify(
+            proof,
+            verifier,
+            joinSplitPubKey,
+            jsdesc.randomSeed,
+            jsdesc.macs,
+            jsdesc.nullifiers,
+            jsdesc.commitments,
+            jsdesc.vpub_old,
+            jsdesc.vpub_new,
+            jsdesc.anchor
+        );
+    }
+
+    bool operator()(const libzcash::GrothProof& proof) const
+    {
+        uint256 h_sig = params.h_sig(jsdesc.randomSeed, jsdesc.nullifiers, joinSplitPubKey);
+
+        return librustzcash_sprout_verify(
+            proof.begin(),
+            jsdesc.anchor.begin(),
+            h_sig.begin(),
+            jsdesc.macs[0].begin(),
+            jsdesc.macs[1].begin(),
+            jsdesc.nullifiers[0].begin(),
+            jsdesc.nullifiers[1].begin(),
+            jsdesc.commitments[0].begin(),
+            jsdesc.commitments[1].begin(),
+            jsdesc.vpub_old,
+            jsdesc.vpub_new
+        );
+    }
+};
+
+bool JSDescription::Verify(
+    ZCJoinSplit& params,
+    libzcash::ProofVerifier& verifier,
+    const uint256& joinSplitPubKey
+) const {
+    auto pv = SproutProofVerifier(params, verifier, joinSplitPubKey, *this);
+    return boost::apply_visitor(pv, proof);
+}
+
+uint256 JSDescription::h_sig(ZCJoinSplit& params, const uint256& joinSplitPubKey) const
+{
+    return params.h_sig(randomSeed, nullifiers, joinSplitPubKey);
+}
+
+std::string COutPoint::ToString() const
+{
+    return strprintf("COutPoint(%s, %u)", hash.ToString().substr(0,10), n);
+}
+
+std::string COutPoint::ToStringShort() const
+{
+    return strprintf("%s-%u", hash.ToString().substr(0,64), n);
+}
+
+uint256 COutPoint::GetHash()
+{
+    return Hash(BEGIN(hash), END(hash), BEGIN(n), END(n));
+}
+
+std::string SaplingOutPoint::ToString() const
+{
+    return strprintf("SaplingOutPoint(%s, %u)", hash.ToString().substr(0, 10), n);
+}
+
+CTxIn::CTxIn(COutPoint prevoutIn, CScript scriptSigIn, uint32_t nSequenceIn)
+{
+    prevout = prevoutIn;
+    scriptSig = scriptSigIn;
+    nSequence = nSequenceIn;
+}
+
+CTxIn::CTxIn(uint256 hashPrevTx, uint32_t nOut, CScript scriptSigIn, uint32_t nSequenceIn)
+{
+    prevout = COutPoint(hashPrevTx, nOut);
+    scriptSig = scriptSigIn;
+    nSequence = nSequenceIn;
+}
+
+std::string CTxIn::ToString() const
+{
+    std::string str;
+    str += "CTxIn(";
+    str += prevout.ToString();
+    if (prevout.IsNull())
+        str += strprintf(", coinbase %s", HexStr(scriptSig));
+    else
+        str += strprintf(", scriptSig=%s", HexStr(scriptSig).substr(0, 24));
+    if (nSequence != std::numeric_limits<unsigned int>::max())
+        str += strprintf(", nSequence=%u", nSequence);
+    str += ")";
+    return str;
+}
+
+CTxOut::CTxOut(const CAmount& nValueIn, CScript scriptPubKeyIn)
+{
+    nValue = nValueIn;
+    scriptPubKey = scriptPubKeyIn;
+    nRounds = -10;
+}
+
+uint256 CTxOut::GetHash() const
+{
+    return SerializeHash(*this);
+}
+
+std::string CTxOut::ToString() const
+{
+    return strprintf("CTxOut(nValue=%d.%08d, scriptPubKey=%s)", nValue / COIN, nValue % COIN, HexStr(scriptPubKey).substr(0, 30));
+}
+
+CMutableTransaction::CMutableTransaction() : nVersion(CTransaction::SPROUT_MIN_CURRENT_VERSION), fOverwintered(false), nVersionGroupId(0), nExpiryHeight(0), nLockTime(0), valueBalance(0) {}
+CMutableTransaction::CMutableTransaction(const CTransaction& tx) : nVersion(tx.nVersion), fOverwintered(tx.fOverwintered), nVersionGroupId(tx.nVersionGroupId), nExpiryHeight(tx.nExpiryHeight),
+                                                                   vin(tx.vin), vout(tx.vout), nLockTime(tx.nLockTime),
+                                                                   valueBalance(tx.valueBalance), vShieldedSpend(tx.vShieldedSpend), vShieldedOutput(tx.vShieldedOutput),
+                                                                   vjoinsplit(tx.vjoinsplit), joinSplitPubKey(tx.joinSplitPubKey), joinSplitSig(tx.joinSplitSig),
+                                                                   bindingSig(tx.bindingSig)
+{
+    
+}
+
+uint25
