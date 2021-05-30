a81485c9b74d1bd3189678ce0e594f33f2255497
chatjd <125004618+chatjd@users.noreply.github.com>
2021-05-30 13:40:30 +0000

    Update IntelGalileo.md

--- src/rpcmasternode-budget.cpp
+++ src/rpcmasternode-budget.cpp
@@ -787,4 +787,163 @@ UniValue mnbudgetrawvote(const UniValue& params, bool fHelp)
     if (fHelp || params.size() != 6)
         throw runtime_error(
             "mnbudgetrawvote \"masternode-tx-hash\" masternode-tx-index \"proposal-hash\" yes|no time \"vote-sig\"\n"
-            "\nCompile and relay a proposal vote with provided external signature instead of signing vote internally
+            "\nCompile and relay a proposal vote with provided external signature instead of signing vote internally\n"
+
+            "\nArguments:\n"
+            "1. \"masternode-tx-hash\"  (string, required) Transaction hash for the masternode\n"
+            "2. masternode-tx-index   (numeric, required) Output index for the masternode\n"
+            "3. \"proposal-hash\"       (string, required) Proposal vote hash\n"
+            "4. yes|no                (boolean, required) Vote to cast\n"
+            "5. time                  (numeric, required) Time since epoch in seconds\n"
+            "6. \"vote-sig\"            (string, required) External signature\n"
+
+            "\nResult:\n"
+            "\"status\"     (string) Vote status or error message\n"
+            "\nExamples:\n" +
+            HelpExampleCli("mnbudgetrawvote", "") + HelpExampleRpc("mnbudgetrawvote", ""));
+
+    uint256 hashMnTx = ParseHashV(params[0], "mn tx hash");
+    int nMnTxIndex = params[1].get_int();
+    CTxIn vin = CTxIn(hashMnTx, nMnTxIndex);
+
+    uint256 hashProposal = ParseHashV(params[2], "Proposal hash");
+    std::string strVote = params[3].get_str();
+
+    if (strVote != "yes" && strVote != "no") return "You can only vote 'yes' or 'no'";
+    int nVote = VOTE_ABSTAIN;
+    if (strVote == "yes") nVote = VOTE_YES;
+    if (strVote == "no") nVote = VOTE_NO;
+
+    int64_t nTime = params[4].get_int64();
+    std::string strSig = params[5].get_str();
+    bool fInvalid = false;
+    vector<unsigned char> vchSig = DecodeBase64(strSig.c_str(), &fInvalid);
+
+    if (fInvalid)
+        throw JSONRPCError(RPC_INVALID_ADDRESS_OR_KEY, "Malformed base64 encoding");
+
+    CMasternode* pmn = mnodeman.Find(vin);
+    if (pmn == NULL) {
+        return "Failure to find masternode in list : " + vin.ToString();
+    }
+
+    CBudgetVote vote(vin, hashProposal, nVote);
+    vote.nTime = nTime;
+    vote.vchSig = vchSig;
+
+    if (!vote.SignatureValid(true)) {
+        return "Failure to verify signature.";
+    }
+
+    std::string strError = "";
+    if (budget.UpdateProposal(vote, NULL, strError)) {
+        budget.mapSeenMasternodeBudgetVotes.insert(make_pair(vote.GetHash(), vote));
+        vote.Relay();
+        return "Voted successfully";
+    } else {
+        return "Error voting : " + strError;
+    }
+}
+
+UniValue mnfinalbudget(const UniValue& params, bool fHelp)
+{
+    string strCommand;
+    if (params.size() >= 1)
+        strCommand = params[0].get_str();
+
+    if (fHelp ||
+        (strCommand != "suggest" && strCommand != "vote-many" && strCommand != "vote" && strCommand != "show" && strCommand != "getvotes"))
+        throw runtime_error(
+            "mnfinalbudget \"command\"... ( \"passphrase\" )\n"
+            "Vote or show current budgets\n"
+            "\nAvailable commands:\n"
+            "  vote-many   - Vote on a finalized budget\n"
+            "  vote        - Vote on a finalized budget\n"
+            "  show        - Show existing finalized budgets\n"
+            "  getvotes     - Get vote information for each finalized budget\n");
+
+    if (strCommand == "vote-many") {
+        if (params.size() != 2)
+            throw runtime_error("Correct usage is 'mnfinalbudget vote-many BUDGET_HASH'");
+
+        std::string strHash = params[1].get_str();
+        uint256 hash(uint256S(strHash));
+
+        int success = 0;
+        int failed = 0;
+
+        UniValue resultsObj(UniValue::VOBJ);
+
+        BOOST_FOREACH (CMasternodeConfig::CMasternodeEntry mne, masternodeConfig.getEntries()) {
+            std::string errorMessage;
+            std::vector<unsigned char> vchMasterNodeSignature;
+            std::string strMasterNodeSignMessage;
+
+            CPubKey pubKeyCollateralAddress;
+            CKey keyCollateralAddress;
+            CPubKey pubKeyMasternode;
+            CKey keyMasternode;
+
+            UniValue statusObj(UniValue::VOBJ);
+
+            if (!obfuScationSigner.SetKey(mne.getPrivKey(), errorMessage, keyMasternode, pubKeyMasternode)) {
+                failed++;
+                statusObj.push_back(Pair("result", "failed"));
+                statusObj.push_back(Pair("errorMessage", "Masternode signing error, could not set key correctly: " + errorMessage));
+                resultsObj.push_back(Pair(mne.getAlias(), statusObj));
+                continue;
+            }
+
+            CMasternode* pmn = mnodeman.Find(pubKeyMasternode);
+            if (pmn == NULL) {
+                failed++;
+                statusObj.push_back(Pair("result", "failed"));
+                statusObj.push_back(Pair("errorMessage", "Can't find masternode by pubkey"));
+                resultsObj.push_back(Pair(mne.getAlias(), statusObj));
+                continue;
+            }
+
+
+            CFinalizedBudgetVote vote(pmn->vin, hash);
+            if (!vote.Sign(keyMasternode, pubKeyMasternode)) {
+                failed++;
+                statusObj.push_back(Pair("result", "failed"));
+                statusObj.push_back(Pair("errorMessage", "Failure to sign."));
+                resultsObj.push_back(Pair(mne.getAlias(), statusObj));
+                continue;
+            }
+
+            std::string strError = "";
+            if (budget.UpdateFinalizedBudget(vote, NULL, strError)) {
+                budget.mapSeenFinalizedBudgetVotes.insert(make_pair(vote.GetHash(), vote));
+                vote.Relay();
+                success++;
+                statusObj.push_back(Pair("result", "success"));
+            } else {
+                failed++;
+                statusObj.push_back(Pair("result", strError.c_str()));
+            }
+
+            resultsObj.push_back(Pair(mne.getAlias(), statusObj));
+        }
+
+        UniValue returnObj(UniValue::VOBJ);
+        returnObj.push_back(Pair("overall", strprintf("Voted successfully %d time(s) and failed %d time(s).", success, failed)));
+        returnObj.push_back(Pair("detail", resultsObj));
+
+        return returnObj;
+    }
+
+    if (strCommand == "vote") {
+        if (params.size() != 2)
+            throw runtime_error("Correct usage is 'mnfinalbudget vote BUDGET_HASH'");
+
+        std::string strHash = params[1].get_str();
+        uint256 hash;
+        hash = ArithToUint256(arith_uint256(strHash));
+
+        CPubKey pubKeyMasternode;
+        CKey keyMasternode;
+        std::string errorMessage;
+
+        if (!obfuScationSigner.SetKey(strMasterNodePrivKey, errorMessage, keyMasternode, pubKeyMasternode)
