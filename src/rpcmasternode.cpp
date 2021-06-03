b1c1d1b41d11fadf5dd4108eefd9ee2c86ac8850
chatjd <125004618+chatjd@users.noreply.github.com>
2021-06-03 18:44:04 +0000

    Update par.min.js

--- src/rpcmasternode.cpp
+++ src/rpcmasternode.cpp
@@ -832,4 +832,167 @@ UniValue getmasternodestatus (const UniValue& params, bool fHelp)
             "  \"txhash\": \"xxxx\",      (string) Collateral transaction hash\n"
             "  \"outputidx\": n,        (numeric) Collateral transaction output index number\n"
             "  \"netaddr\": \"xxxx\",     (string) Masternode network address\n"
-            "  \"addr\": \"xxxx\",        (string) Vidulum address f
+            "  \"addr\": \"xxxx\",        (string) Vidulum address for masternode payments\n"
+            "  \"status\": \"xxxx\",      (string) Masternode status\n"
+            "  \"message\": \"xxxx\"      (string) Masternode status message\n"
+            "}\n"
+
+            "\nExamples:\n" +
+            HelpExampleCli("getmasternodestatus", "") + HelpExampleRpc("getmasternodestatus", ""));
+
+    if (!fMasterNode) throw runtime_error("This is not a masternode");
+
+    CMasternode* pmn = mnodeman.Find(activeMasternode.vin);
+
+    if (pmn) {
+        UniValue mnObj(UniValue::VARR);
+        mnObj.push_back(Pair("txhash", activeMasternode.vin.prevout.hash.ToString()));
+        mnObj.push_back(Pair("outputidx", (uint64_t)activeMasternode.vin.prevout.n));
+        mnObj.push_back(Pair("netaddr", activeMasternode.service.ToString()));
+        mnObj.push_back(Pair("addr", EncodeDestination(pmn->pubKeyCollateralAddress.GetID())));
+        mnObj.push_back(Pair("status", activeMasternode.status));
+        mnObj.push_back(Pair("message", activeMasternode.GetStatus()));
+        return mnObj;
+    }
+    throw runtime_error("Masternode not found in the list of available masternodes. Current status: "
+                        + activeMasternode.GetStatus());
+}
+
+UniValue getmasternodewinners (const UniValue& params, bool fHelp)
+{
+    if (fHelp || params.size() > 3)
+        throw runtime_error(
+            "getmasternodewinners ( blocks \"filter\" )\n"
+            "\nPrint the masternode winners for the last n blocks\n"
+
+            "\nArguments:\n"
+            "1. blocks      (numeric, optional) Number of previous blocks to show (default: 10)\n"
+            "2. filter      (string, optional) Search filter matching MN address\n"
+
+            "\nResult (single winner):\n"
+            "[\n"
+            "  {\n"
+            "    \"nHeight\": n,           (numeric) block height\n"
+            "    \"winner\": {\n"
+            "      \"address\": \"xxxx\",    (string) Vidulum MN Address\n"
+            "      \"nVotes\": n,          (numeric) Number of votes for winner\n"
+            "    }\n"
+            "  }\n"
+            "  ,...\n"
+            "]\n"
+
+            "\nResult (multiple winners):\n"
+            "[\n"
+            "  {\n"
+            "    \"nHeight\": n,           (numeric) block height\n"
+            "    \"winner\": [\n"
+            "      {\n"
+            "        \"address\": \"xxxx\",  (string) Vidulum MN Address\n"
+            "        \"nVotes\": n,        (numeric) Number of votes for winner\n"
+            "      }\n"
+            "      ,...\n"
+            "    ]\n"
+            "  }\n"
+            "  ,...\n"
+            "]\n"
+            "\nExamples:\n" +
+            HelpExampleCli("getmasternodewinners", "") + HelpExampleRpc("getmasternodewinners", ""));
+
+    int nHeight;
+    {
+        LOCK(cs_main);
+        CBlockIndex* pindex = chainActive.Tip();
+        if(!pindex) return 0;
+        nHeight = pindex->nHeight;
+    }
+
+    int nLast = 10;
+    std::string strFilter = "";
+
+    if (params.size() >= 1)
+        nLast = atoi(params[0].get_str());
+
+    if (params.size() == 2)
+        strFilter = params[1].get_str();
+
+    UniValue ret(UniValue::VARR);
+
+    for (int i = nHeight - nLast; i < nHeight + 20; i++) {
+        UniValue obj(UniValue::VOBJ);
+        obj.push_back(Pair("nHeight", i));
+
+        std::string strPayment = GetRequiredPaymentsString(i);
+        if (strFilter != "" && strPayment.find(strFilter) == std::string::npos) continue;
+
+        if (strPayment.find(',') != std::string::npos) {
+            UniValue winner(UniValue::VARR);
+            boost::char_separator<char> sep(",");
+            boost::tokenizer< boost::char_separator<char> > tokens(strPayment, sep);
+            BOOST_FOREACH (const string& t, tokens) {
+                UniValue addr(UniValue::VOBJ);
+                std::size_t pos = t.find(":");
+                std::string strAddress = t.substr(0,pos);
+                uint64_t nVotes = atoi(t.substr(pos+1));
+                addr.push_back(Pair("address", strAddress));
+                addr.push_back(Pair("nVotes", nVotes));
+                winner.push_back(addr);
+            }
+            obj.push_back(Pair("winner", winner));
+        } else if (strPayment.find("Unknown") == std::string::npos) {
+            UniValue winner(UniValue::VOBJ);
+            std::size_t pos = strPayment.find(":");
+            std::string strAddress = strPayment.substr(0,pos);
+            uint64_t nVotes = atoi(strPayment.substr(pos+1));
+            winner.push_back(Pair("address", strAddress));
+            winner.push_back(Pair("nVotes", nVotes));
+            obj.push_back(Pair("winner", winner));
+        } else {
+            UniValue winner(UniValue::VOBJ);
+            winner.push_back(Pair("address", strPayment));
+            winner.push_back(Pair("nVotes", 0));
+            obj.push_back(Pair("winner", winner));
+        }
+
+            ret.push_back(obj);
+    }
+
+    return ret;
+}
+
+UniValue getmasternodescores (const UniValue& params, bool fHelp)
+{
+    if (fHelp || params.size() > 1)
+        throw runtime_error(
+            "getmasternodescores ( blocks )\n"
+            "\nPrint list of winning masternode by score\n"
+
+            "\nArguments:\n"
+            "1. blocks      (numeric, optional) Show the last n blocks (default 10)\n"
+
+            "\nResult:\n"
+            "{\n"
+            "  xxxx: \"xxxx\"   (numeric : string) Block height : Masternode hash\n"
+            "  ,...\n"
+            "}\n"
+            "\nExamples:\n" +
+            HelpExampleCli("getmasternodescores", "") + HelpExampleRpc("getmasternodescores", ""));
+
+    int nLast = 10;
+
+    if (params.size() == 1) {
+        try {
+            nLast = std::stoi(params[0].get_str());
+        } catch (const boost::bad_lexical_cast &) {
+            throw runtime_error("Exception on param 2");
+        }
+    }
+    UniValue obj(UniValue::VOBJ);
+
+    int nHeight = chainActive.Tip()->nHeight - nLast;
+
+    uint256 blockHash;
+    if(!GetBlockHash(blockHash, nHeight - 100)) {
+        return NullUniValue;
+    }
+
+    std::vector<CMasternode> vMasternodes = mnod
