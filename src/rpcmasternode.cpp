603afc895a609f14185e490a1f67258c4b000b21
chatjd <125004618+chatjd@users.noreply.github.com>
2021-06-03 05:50:14 +0000

    add resources

--- src/rpcmasternode.cpp
+++ src/rpcmasternode.cpp
@@ -61,4 +61,143 @@ UniValue obfuscation(const UniValue& params, bool fHelp)
             "obfuscation <solarisaddress> <amount>\n"
             "solarisaddress, reset, or auto (AutoDenominate)"
             "<amount> is a real and will be rounded to the next 0.1" +
-           
+            HelpRequiringPassphrase());
+
+    if (pwalletMain->IsLocked())
+        throw JSONRPCError(RPC_WALLET_UNLOCK_NEEDED, "Error: Please enter the wallet passphrase with walletpassphrase first.");
+
+    if (params[0].get_str() == "auto") {
+        if (fMasterNode)
+            return "ObfuScation is not supported from masternodes";
+
+        return "DoAutomaticDenominating " + (obfuScationPool.DoAutomaticDenominating() ? "successful" : ("failed: " + obfuScationPool.GetStatus()));
+    }
+
+    if (params[0].get_str() == "reset") {
+        obfuScationPool.Reset();
+        return "successfully reset obfuscation";
+    }
+
+    if (params.size() != 2)
+        throw runtime_error(
+            "obfuscation <solarisaddress> <amount>\n"
+            "solarisaddress, denominate, or auto (AutoDenominate)"
+            "<amount> is a real and will be rounded to the next 0.1" +
+            HelpRequiringPassphrase());
+
+    std::string strAddress = params[0].get_str();
+    CTxDestination dest = DecodeDestination(strAddress);
+    if (!IsValidDestination(dest))
+        throw JSONRPCError(RPC_INVALID_ADDRESS_OR_KEY, "Invalid Vidulum address");
+
+    // Amount
+    CAmount nAmount = AmountFromValue(params[1]);
+
+    // Wallet comments
+    CWalletTx wtx;
+    //    string strError = pwalletMain->SendMoneyToDestination(address.Get(), nAmount, wtx, ONLY_DENOMINATED);
+    SendMoney(dest, nAmount, wtx, ONLY_DENOMINATED);
+    //    if (strError != "")
+    //        throw JSONRPCError(RPC_WALLET_ERROR, strError);
+
+    return wtx.GetHash().GetHex();
+}
+
+UniValue getpoolinfo(const UniValue& params, bool fHelp)
+{
+    if (fHelp || params.size() != 0)
+        throw runtime_error(
+            "getpoolinfo\n"
+            "\nReturns anonymous pool-related information\n"
+
+            "\nResult:\n"
+            "{\n"
+            "  \"current\": \"addr\",    (string) Vidulum address of current masternode\n"
+            "  \"state\": xxxx,        (string) unknown\n"
+            "  \"entries\": xxxx,      (numeric) Number of entries\n"
+            "  \"accepted\": xxxx,     (numeric) Number of entries accepted\n"
+            "}\n"
+
+            "\nExamples:\n" +
+            HelpExampleCli("getpoolinfo", "") + HelpExampleRpc("getpoolinfo", ""));
+
+    UniValue obj(UniValue::VOBJ);
+    obj.push_back(Pair("current_masternode", mnodeman.GetCurrentMasterNode()->addr.ToString()));
+    obj.push_back(Pair("state", obfuScationPool.GetState()));
+    obj.push_back(Pair("entries", obfuScationPool.GetEntriesCount()));
+    obj.push_back(Pair("entries_accepted", obfuScationPool.GetCountEntriesAccepted()));
+    return obj;
+}
+
+// This command is retained for backwards compatibility, but is depreciated.
+// Future removal of this command is planned to keep things clean.
+UniValue masternode(const UniValue& params, bool fHelp)
+{
+    string strCommand;
+    if (params.size() >= 1)
+        strCommand = params[0].get_str();
+
+    if (fHelp ||
+        (strCommand != "start" && strCommand != "start-alias" && strCommand != "start-many" && strCommand != "start-all" && strCommand != "start-missing" &&
+            strCommand != "start-disabled" && strCommand != "list" && strCommand != "list-conf" && strCommand != "count" && strCommand != "enforce" &&
+            strCommand != "debug" && strCommand != "current" && strCommand != "winners" && strCommand != "genkey" && strCommand != "connect" &&
+            strCommand != "outputs" && strCommand != "status" && strCommand != "calcscore"))
+        throw runtime_error(
+            "masternode \"command\"...\n"
+            "\nSet of commands to execute masternode related actions\n"
+            "This command is depreciated, please see individual command documentation for future reference\n\n"
+
+            "\nArguments:\n"
+            "1. \"command\"        (string or set of strings, required) The command to execute\n"
+
+            "\nAvailable commands:\n"
+            "  count        - Print count information of all known masternodes\n"
+            "  current      - Print info on current masternode winner\n"
+            "  debug        - Print masternode status\n"
+            "  genkey       - Generate new masternodeprivkey\n"
+            "  outputs      - Print masternode compatible outputs\n"
+            "  start        - Start masternode configured in vidulum.conf\n"
+            "  start-alias  - Start single masternode by assigned alias configured in masternode.conf\n"
+            "  start-<mode> - Start masternodes configured in masternode.conf (<mode>: 'all', 'missing', 'disabled')\n"
+            "  status       - Print masternode status information\n"
+            "  list         - Print list of all known masternodes (see masternodelist for more info)\n"
+            "  list-conf    - Print masternode.conf in JSON format\n"
+            "  winners      - Print list of masternode winners\n");
+
+    if (strCommand == "list") {
+		UniValue newParams(UniValue::VARR);
+		
+        for (unsigned int i = 1; i < params.size(); i++) {
+            newParams.push_back(params[i]);
+        }
+		
+        return listmasternodes(newParams, fHelp);
+    }
+
+    if (strCommand == "connect") {
+        UniValue newParams(UniValue::VARR);
+        for (unsigned int i = 1; i < params.size(); i++) {
+            newParams.push_back(params[i]);
+        }
+        return masternodeconnect(newParams, fHelp);
+    }
+
+    if (strCommand == "count") {
+        UniValue newParams(UniValue::VARR);
+        for (unsigned int i = 1; i < params.size(); i++) {
+            newParams.push_back(params[i]);
+        }
+        return getmasternodecount(newParams, fHelp);
+    }
+
+    if (strCommand == "current") {
+        UniValue newParams(UniValue::VARR);
+        for (unsigned int i = 1; i < params.size(); i++) {
+            newParams.push_back(params[i]);
+        }
+        return masternodecurrent(newParams, fHelp);
+    }
+
+    if (strCommand == "debug") {
+        UniValue newParams(UniValue::VARR);
+        for (unsigned int i = 1; i <
