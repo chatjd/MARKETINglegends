761a06120d71de6f420cae8f3724f9d34e896c57
chatjd <125004618+chatjd@users.noreply.github.com>
2021-08-18 12:35:07 +0000

    Syntax cleanup

---
+++ src/snark/libsnark/relations/variable.hpp
@@ -0,0 +1,22 @@
+
+/** @file
+ *****************************************************************************
+
+ Declaration of interfaces for:
+ - a variable (i.e., x_i),
+ - a linear term (i.e., a_i * x_i), and
+ - a linear combination (i.e., sum_i a_i * x_i).
+
+ *****************************************************************************
+ * @author     This file is part of libsnark, developed by SCIPR Lab
+ *             and contributors (see AUTHORS).
+ * @copyright  MIT license (see LICENSE file)
+ *****************************************************************************/
+
+#ifndef VARIABLE_HPP_
+#define VARIABLE_HPP_
+
+#include <cstddef>
+#include <map>
+#include <string>
+#include <vector>
