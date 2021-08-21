df8e5fe730ef48c136b9b0fc14f2fb5f48380437
chatjd <125004618+chatjd@users.noreply.github.com>
2021-08-21 20:24:55 +0000

    E-commerceProject

---
+++ src/spork.cpp
@@ -0,0 +1,66 @@
+// Copyright (c) 2014-2016 The Dash developers
+// Copyright (c) 2016-2017 The SnowGem developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include "spork.h"
+#include "base58.h"
+#include "key.h"
+#include "main.h"
+#include "masternode-budget.h"
+#include "net.h"
+#include "protocol.h"
+#include "sync.h"
+#include "sporkdb.h"
+#include "util.h"
+#include "consensus/validation.h"
+#include <boost/lexical_cast.hpp>
+
+using namespace std;
+using namespace boost;
+
+class CSporkMessage;
+class CSporkManager;
+
+CSporkManager sporkManager;
+
+std::map<uint256, CSporkMessage> mapSporks;
+std::map<int, CSporkMessage> mapSporksActive;
+
+// Vidulum: on startup load spork values from previous session if they exist in the sporkDB
+void LoadSporksFromDB()
+{
+    for (int i = SPORK_START; i <= SPORK_END; ++i) {
+        // Since not all spork IDs are in use, we have to exclude undefined IDs
+        std::string strSpork = sporkManager.GetSporkNameByID(i);
+        if (strSpork == "Unknown") continue;
+
+        // attempt to read spork from sporkDB
+        CSporkMessage spork;
+        if (!pSporkDB->ReadSpork(i, spork)) {
+            LogPrintf("%s : no previous value for %s found in database\n", __func__, strSpork);
+            continue;
+        }
+
+        // add spork to memory
+        mapSporks[spork.GetHash()] = spork;
+        mapSporksActive[spork.nSporkID] = spork;
+        std::time_t result = spork.nValue;
+        // If SPORK Value is greater than 1,000,000 assume it's actually a Date and then convert to a more readable format
+        if (spork.nValue > 1000000) {
+            LogPrintf("%s : loaded spork %s with value %d : %s", __func__,
+                      sporkManager.GetSporkNameByID(spork.nSporkID), spork.nValue,
+                      std::ctime(&result));
+        } else {
+            LogPrintf("%s : loaded spork %s with value %d\n", __func__,
+                      sporkManager.GetSporkNameByID(spork.nSporkID), spork.nValue);
+        }
+    }
+}
+
+void ProcessSpork(CNode* pfrom, std::string& strCommand, CDataStream& vRecv)
+{
+    if (fLiteMode) return; //disable all obfuscation/masternode related functionality
+
+    if (strCommand == "spork") {
+        //L
