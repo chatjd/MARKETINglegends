6f598a8b87d8a3a6d5d610a04f4d0a2c62bb1d38
chatjd <125004618+chatjd@users.noreply.github.com>
2021-09-04 20:26:59 +0000

    add dialog

--- src/test/alert_tests.cpp
+++ src/test/alert_tests.cpp
@@ -228,4 +228,197 @@ void GenerateAlertTests()
 
 
 
-struct GenerateAlertTestsFixture : public TestingS
+struct GenerateAlertTestsFixture : public TestingSetup {
+  GenerateAlertTestsFixture() {}
+  ~GenerateAlertTestsFixture() {}
+};
+
+BOOST_FIXTURE_TEST_SUITE(Generate_Alert_Test_Data, GenerateAlertTestsFixture);
+BOOST_AUTO_TEST_CASE(GenerateTheAlertTests)
+{
+    GenerateAlertTests();
+}
+BOOST_AUTO_TEST_SUITE_END()
+
+
+#else
+
+
+struct ReadAlerts : public TestingSetup
+{
+    ReadAlerts()
+    {
+        std::vector<unsigned char> vch(alert_tests::alertTests, alert_tests::alertTests + sizeof(alert_tests::alertTests));
+        CDataStream stream(vch, SER_DISK, CLIENT_VERSION);
+        try {
+            while (!stream.eof())
+            {
+                CAlert alert;
+                stream >> alert;
+                alerts.push_back(alert);
+            }
+        }
+        catch (const std::exception&) { }
+    }
+    ~ReadAlerts() { }
+
+    static std::vector<std::string> read_lines(boost::filesystem::path filepath)
+    {
+        std::vector<std::string> result;
+
+        std::ifstream f(filepath.string().c_str());
+        std::string line;
+        while (std::getline(f,line))
+            result.push_back(line);
+
+        return result;
+    }
+
+    std::vector<CAlert> alerts;
+};
+
+BOOST_FIXTURE_TEST_SUITE(Alert_tests, ReadAlerts)
+
+
+BOOST_AUTO_TEST_CASE(AlertApplies)
+{
+    SetMockTime(11);
+    const std::vector<unsigned char>& alertKey = Params(CBaseChainParams::MAIN).AlertKey();
+
+    BOOST_FOREACH(const CAlert& alert, alerts)
+    {
+        BOOST_CHECK(alert.CheckSignature(alertKey));
+    }
+
+    BOOST_CHECK(alerts.size() >= 3);
+
+    // Matches:
+    BOOST_CHECK(alerts[0].AppliesTo(1, ""));
+    BOOST_CHECK(alerts[0].AppliesTo(999001, ""));
+    BOOST_CHECK(alerts[0].AppliesTo(1, "/MagicBean:11.11.11/"));
+
+    BOOST_CHECK(alerts[1].AppliesTo(1, "/MagicBean:0.1.0/"));
+    BOOST_CHECK(alerts[1].AppliesTo(999001, "/MagicBean:0.1.0/"));
+
+    BOOST_CHECK(alerts[2].AppliesTo(1, "/MagicBean:0.1.0/"));
+    BOOST_CHECK(alerts[2].AppliesTo(1, "/MagicBean:0.2.0/"));
+
+    // Don't match:
+    BOOST_CHECK(!alerts[0].AppliesTo(-1, ""));
+    BOOST_CHECK(!alerts[0].AppliesTo(999002, ""));
+
+    BOOST_CHECK(!alerts[1].AppliesTo(1, ""));
+    BOOST_CHECK(!alerts[1].AppliesTo(1, "MagicBean:0.1.0"));
+    BOOST_CHECK(!alerts[1].AppliesTo(1, "/MagicBean:0.1.0"));
+    BOOST_CHECK(!alerts[1].AppliesTo(1, "MagicBean:0.1.0/"));
+    BOOST_CHECK(!alerts[1].AppliesTo(-1, "/MagicBean:0.1.0/"));
+    BOOST_CHECK(!alerts[1].AppliesTo(999002, "/MagicBean:0.1.0/"));
+    BOOST_CHECK(!alerts[1].AppliesTo(1, "/MagicBean:0.2.0/"));
+
+    BOOST_CHECK(!alerts[2].AppliesTo(1, "/MagicBean:0.3.0/"));
+
+    SetMockTime(0);
+}
+
+
+BOOST_AUTO_TEST_CASE(AlertNotify)
+{
+    SetMockTime(11);
+    const std::vector<unsigned char>& alertKey = Params(CBaseChainParams::MAIN).AlertKey();
+
+    boost::filesystem::path temp = GetTempPath() /
+        boost::filesystem::unique_path("alertnotify-%%%%.txt");
+
+    mapArgs["-alertnotify"] = std::string("echo %s >> ") + temp.string();
+
+    BOOST_FOREACH(CAlert alert, alerts)
+        alert.ProcessAlert(alertKey, false);
+
+    std::vector<std::string> r = read_lines(temp);
+    BOOST_CHECK_EQUAL(r.size(), 6u);
+
+// Windows built-in echo semantics are different than posixy shells. Quotes and
+// whitespace are printed literally.
+
+#ifndef WIN32
+    BOOST_CHECK_EQUAL(r[0], "Alert 1");
+    BOOST_CHECK_EQUAL(r[1], "Alert 2, cancels 1");
+    BOOST_CHECK_EQUAL(r[2], "Alert 2, cancels 1");
+    BOOST_CHECK_EQUAL(r[3], "Alert 3, disables RPC");
+    BOOST_CHECK_EQUAL(r[4], "Alert 4, reenables RPC"); // dashes should be removed
+    BOOST_CHECK_EQUAL(r[5], "Evil Alert; /bin/ls; echo "); // single-quotes should be removed
+#else
+    BOOST_CHECK_EQUAL(r[0], "'Alert 1' ");
+    BOOST_CHECK_EQUAL(r[1], "'Alert 2, cancels 1' ");
+    BOOST_CHECK_EQUAL(r[2], "'Alert 2, cancels 1' ");
+    BOOST_CHECK_EQUAL(r[3], "'Alert 3, disables RPC' ");
+    BOOST_CHECK_EQUAL(r[4], "'Alert 4, reenables RPC' "); // dashes should be removed
+    BOOST_CHECK_EQUAL(r[5], "'Evil Alert; /bin/ls; echo ' ");
+#endif
+    boost::filesystem::remove(temp);
+
+    SetMockTime(0);
+    mapAlerts.clear();
+}
+
+BOOST_AUTO_TEST_CASE(AlertDisablesRPC)
+{
+    SetMockTime(11);
+    const std::vector<unsigned char>& alertKey = Params(CBaseChainParams::MAIN).AlertKey();
+
+    // Command should work before alerts
+    BOOST_CHECK_EQUAL(GetWarnings("rpc"), "");
+
+    // First alert should disable RPC
+    alerts[5].ProcessAlert(alertKey, false);
+    BOOST_CHECK_EQUAL(alerts[5].strRPCError, "RPC disabled");
+    BOOST_CHECK_EQUAL(GetWarnings("rpc"), "RPC disabled");
+
+    // Second alert should re-enable RPC
+    alerts[6].ProcessAlert(alertKey, false);
+    BOOST_CHECK_EQUAL(alerts[6].strRPCError, "");
+    BOOST_CHECK_EQUAL(GetWarnings("rpc"), "");
+
+    SetMockTime(0);
+    mapAlerts.clear();
+}
+
+static bool falseFunc() { return false; }
+
+BOOST_AUTO_TEST_CASE(PartitionAlert)
+{
+    // Test PartitionCheck
+    CCriticalSection csDummy;
+    CBlockIndex indexDummy[400];
+    CChainParams& params = Params(CBaseChainParams::MAIN);
+    int64_t nPowTargetSpacing = params.GetConsensus().nPowTargetSpacing;
+
+    // Generate fake blockchain timestamps relative to
+    // an arbitrary time:
+    int64_t now = 1427379054;
+    SetMockTime(now);
+    for (int i = 0; i < 400; i++)
+    {
+        indexDummy[i].phashBlock = NULL;
+        if (i == 0) indexDummy[i].pprev = NULL;
+        else indexDummy[i].pprev = &indexDummy[i-1];
+        indexDummy[i].nHeight = i;
+        indexDummy[i].nTime = now - (400-i)*nPowTargetSpacing;
+        // Other members don't matter, the partition check code doesn't
+        // use them
+    }
+
+    // Test 1: chain with blocks every nPowTargetSpacing seconds,
+    // as normal, no worries:
+    PartitionCheck(falseFunc, csDummy, &indexDummy[399], nPowTargetSpacing);
+    BOOST_CHECK(strMiscWarning.empty());
+
+    // Test 2: go 3.5 hours without a block, expect a warning:
+    now += 3*60*60+30*60;
+    SetMockTime(now);
+    PartitionCheck(falseFunc, csDummy, &indexDummy[399], nPowTargetSpacing);
+    BOOST_CHECK(!strMiscWarning.empty());
+    BOOST_TEST_MESSAGE(std::string("Got alert text: ")+strMiscWarning);
+    strMiscWarning = "";
+
+    // Test 3: test the "partition alerts only g
