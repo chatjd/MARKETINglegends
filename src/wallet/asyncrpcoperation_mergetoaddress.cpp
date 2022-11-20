369d6a0f9a08686cef687ab2d45f71a4aba9bb47
chatjd <125004618+chatjd@users.noreply.github.com>
2022-11-20 06:15:11 +0000

    Updated example files

--- src/wallet/asyncrpcoperation_mergetoaddress.cpp
+++ src/wallet/asyncrpcoperation_mergetoaddress.cpp
@@ -75,4 +75,184 @@ AsyncRPCOperation_mergetoaddress::AsyncRPCOperation_mergetoaddress(
     }
 
     if (std::get<0>(recipient).size() == 0) {
-        throw JSONRPCE
+        throw JSONRPCError(RPC_INVALID_PARAMETER, "Recipient parameter missing");
+    }
+
+    if (sproutNoteInputs.size() > 0 && saplingNoteInputs.size() > 0) {
+        throw JSONRPCError(RPC_INVALID_PARAMETER, "Cannot send from both Sprout and Sapling addresses using z_mergetoaddress");
+    }
+
+    if (sproutNoteInputs.size() > 0 && builder) {
+        throw JSONRPCError(RPC_INVALID_PARAMETER, "Sprout notes are not supported by the TransactionBuilder");
+    }
+
+    isUsingBuilder_ = false;
+    if (builder) {
+        isUsingBuilder_ = true;
+        builder_ = builder.get();
+    }
+
+    toTaddr_ = DecodeDestination(std::get<0>(recipient));
+    isToTaddr_ = IsValidDestination(toTaddr_);
+    isToZaddr_ = false;
+
+    if (!isToTaddr_) {
+        auto address = DecodePaymentAddress(std::get<0>(recipient));
+        if (IsValidPaymentAddress(address)) {
+            isToZaddr_ = true;
+            toPaymentAddress_ = address;
+        } else {
+            throw JSONRPCError(RPC_INVALID_ADDRESS_OR_KEY, "Invalid recipient address");
+        }
+    }
+
+    // Log the context info i.e. the call parameters to z_mergetoaddress
+    if (LogAcceptCategory("zrpcunsafe")) {
+        LogPrint("zrpcunsafe", "%s: z_mergetoaddress initialized (params=%s)\n", getId(), contextInfo.write());
+    } else {
+        LogPrint("zrpc", "%s: z_mergetoaddress initialized\n", getId());
+    }
+
+    // Lock UTXOs
+    lock_utxos();
+    lock_notes();
+
+    // Enable payment disclosure if requested
+    paymentDisclosureMode = fExperimentalMode && GetBoolArg("-paymentdisclosure", false);
+}
+
+AsyncRPCOperation_mergetoaddress::~AsyncRPCOperation_mergetoaddress()
+{
+}
+
+void AsyncRPCOperation_mergetoaddress::main()
+{
+    if (isCancelled()) {
+        unlock_utxos(); // clean up
+        unlock_notes();
+        return;
+    }
+
+    set_state(OperationStatus::EXECUTING);
+    start_execution_clock();
+
+    bool success = false;
+
+#ifdef ENABLE_MINING
+#ifdef ENABLE_WALLET
+    GenerateBitcoins(false, NULL, 0);
+#else
+    GenerateBitcoins(false, 0);
+#endif
+#endif
+
+    try {
+        success = main_impl();
+    } catch (const UniValue& objError) {
+        int code = find_value(objError, "code").get_int();
+        std::string message = find_value(objError, "message").get_str();
+        set_error_code(code);
+        set_error_message(message);
+    } catch (const runtime_error& e) {
+        set_error_code(-1);
+        set_error_message("runtime error: " + string(e.what()));
+    } catch (const logic_error& e) {
+        set_error_code(-1);
+        set_error_message("logic error: " + string(e.what()));
+    } catch (const exception& e) {
+        set_error_code(-1);
+        set_error_message("general exception: " + string(e.what()));
+    } catch (...) {
+        set_error_code(-2);
+        set_error_message("unknown error");
+    }
+
+#ifdef ENABLE_MINING
+#ifdef ENABLE_WALLET
+    GenerateBitcoins(GetBoolArg("-gen", false), pwalletMain, GetArg("-genproclimit", 1));
+#else
+    GenerateBitcoins(GetBoolArg("-gen", false), GetArg("-genproclimit", 1));
+#endif
+#endif
+
+    stop_execution_clock();
+
+    if (success) {
+        set_state(OperationStatus::SUCCESS);
+    } else {
+        set_state(OperationStatus::FAILED);
+    }
+
+    std::string s = strprintf("%s: z_mergetoaddress finished (status=%s", getId(), getStateAsString());
+    if (success) {
+        s += strprintf(", txid=%s)\n", tx_.GetHash().ToString());
+    } else {
+        s += strprintf(", error=%s)\n", getErrorMessage());
+    }
+    LogPrintf("%s", s);
+
+    unlock_utxos(); // clean up
+    unlock_notes(); // clean up
+
+    // !!! Payment disclosure START
+    if (success && paymentDisclosureMode && paymentDisclosureData_.size() > 0) {
+        uint256 txidhash = tx_.GetHash();
+        std::shared_ptr<PaymentDisclosureDB> db = PaymentDisclosureDB::sharedInstance();
+        for (PaymentDisclosureKeyInfo p : paymentDisclosureData_) {
+            p.first.hash = txidhash;
+            if (!db->Put(p.first, p.second)) {
+                LogPrint("paymentdisclosure", "%s: Payment Disclosure: Error writing entry to database for key %s\n", getId(), p.first.ToString());
+            } else {
+                LogPrint("paymentdisclosure", "%s: Payment Disclosure: Successfully added entry to database for key %s\n", getId(), p.first.ToString());
+            }
+        }
+    }
+    // !!! Payment disclosure END
+}
+
+// Notes:
+// 1. #1359 Currently there is no limit set on the number of joinsplits, so size of tx could be invalid.
+// 2. #1277 Spendable notes are not locked, so an operation running in parallel could also try to use them.
+bool AsyncRPCOperation_mergetoaddress::main_impl()
+{
+    assert(isToTaddr_ != isToZaddr_);
+
+    bool isPureTaddrOnlyTx = (sproutNoteInputs_.empty() && saplingNoteInputs_.empty() && isToTaddr_);
+    CAmount minersFee = fee_;
+
+    size_t numInputs = utxoInputs_.size();
+
+    // Check mempooltxinputlimit to avoid creating a transaction which the local mempool rejects
+    size_t limit = (size_t)GetArg("-mempooltxinputlimit", 0);
+    {
+        LOCK(cs_main);
+        if (NetworkUpgradeActive(chainActive.Height() + 1, Params().GetConsensus(), Consensus::UPGRADE_OVERWINTER)) {
+            limit = 0;
+        }
+    }
+    if (limit > 0 && numInputs > limit) {
+        throw JSONRPCError(RPC_WALLET_ERROR,
+                           strprintf("Number of transparent inputs %d is greater than mempooltxinputlimit of %d",
+                                     numInputs, limit));
+    }
+
+    CAmount t_inputs_total = 0;
+    for (MergeToAddressInputUTXO& t : utxoInputs_) {
+        t_inputs_total += std::get<1>(t);
+    }
+
+    CAmount z_inputs_total = 0;
+    for (const MergeToAddressInputSproutNote& t : sproutNoteInputs_) {
+        z_inputs_total += std::get<2>(t);
+    }
+
+    for (const MergeToAddressInputSaplingNote& t : saplingNoteInputs_) {
+        z_inputs_total += std::get<2>(t);
+    }
+
+    CAmount targetAmount = z_inputs_total + t_inputs_total;
+
+    if (targetAmount <= minersFee) {
+        throw JSONRPCError(RPC_WALLET_INSUFFICIENT_FUNDS,
+                           strprintf("Insufficient funds, have %s and miners fee is %s",
+                                     FormatMoney(targetAmount), Fo
